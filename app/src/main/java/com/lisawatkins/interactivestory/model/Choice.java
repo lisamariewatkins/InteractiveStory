751d156901afceb1dad38651ecb033fde588c92f
lisamariewatkins <lisamwatkins@ymail.com>
2017-08-24 11:14:11 -0500

    added MVP pattern

---
+++ app/src/main/java/com/lisawatkins/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.lisawatkins.interactivestory.model;
+
+/**
+ * Created by lisa.watkins on 8/24/2017.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
--- app/src/main/java/com/lisawatkins/interactivestory/ui/MainActivity.java
+++ app/src/main/java/com/lisawatkins/interactivestory/ui/MainActivity.java
@@ -1,16 +1,18 @@
-package com.lisawatkins.interactivestory;
+package com.lisawatkins.interactivestory.ui;
 
-import android.content.pm.ActivityInfo;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.lisawatkins.interactivestory.R;
 
 public class MainActivity extends AppCompatActivity {
     private EditText nameField;
     private Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +25,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = nameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                //  Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory(name);
             }
         });
+    }
+
+    private void startStory(String name) {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(getString(R.string.key_name), name);
+        startActivity(intent);
     }
 }
